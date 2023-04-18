3d92367a9c5f5e6d555df69719155e962773c87a
Moumita-Bhattacharya <125236843+Moumita-Bhattacharya@users.noreply.github.com>
2023-04-18 12:54:59 -0700

    transition screen done for homescreen
    transition screen done for homescreen, other scene left to do

--- Assets/backButton.cs
+++ Assets/backButton.cs
@@ -1,13 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.SearchService;
 using UnityEngine;
 using UnityEngine.SceneManagement; 
 
 public class backButton : MonoBehaviour
 {
-   [SerializeField] private string screen_name;
-   public void backButtonFns()
+    [SerializeField] private string screen_name;
+    [SerializeField] private GameObject endingSceneTransition;
+    public void backButtonFns()
    {
-    SceneManager.LoadScene(screen_name);
-   }
+        if (endingSceneTransition != null)
+        {
+            endingSceneTransition.SetActive(true);
+            Invoke("disableEndTransition", 3f);
+
+            StartCoroutine(loadSceneAfterWait(screen_name, 0.83f));
+        }
+        else
+        {
+            SceneManager.LoadScene(screen_name);
+        }
+    }
+
+    private IEnumerator loadSceneAfterWait(string scene, float time)
+    {
+        yield return new WaitForSeconds(time);
+        SceneManager.LoadScene(scene);
+    }
+
+    private void disableEndTransition()
+    {
+        endingSceneTransition.SetActive(false);
+    }
 }
