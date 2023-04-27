03dbec1257c396c76cf7d082dc45f721f0741bfa
zarhult <cglis001@ucr.edu>
2023-04-27 00:02:22 -0700

    Set up basic layout of puzzle

--- Assets/AdditionPractice.cs
+++ Assets/AdditionPractice.cs
@@ -6,25 +6,38 @@ using System.Collections.Generic;
 
 public class AdditionPractice : MonoBehaviour
 {
+    // Access HelperFunctions
+    private HelperFunctions helperFunctions;
 
+    public Text panel1firstNumber;
+    public Text panel1secondNumber;
+    public Text panel1ans;
+    public Text panel2firstNumber;
+    public Text panel2secondNumber;
+    public Text panel2ans;
+    public Text panel3firstNumber;
+    public Text panel3secondNumber;
+    public Text panel3ans;
+    public Text panel4firstNumber;
+    public Text panel4secondNumber;
+    public Text panel4ans;
+    public Text panel5firstNumber;
+    public Text panel5secondNumber;
+    public Text panel5ans;
+    public Text panel6firstNumber;
+    public Text panel6secondNumber;
+    public Text panel6ans;
 
-    public Text firstNumber;
-    public Text secondNumber;
+    public Text answer1Button;
+    public Text answer2Button;
+    public Text answer3Button;
+    public Text answer4Button;
+    public Text answer5Button;
+    public Text answer6Button;
+    public Text answer7Button;
 
-    public Button answer1Button;
-    public Button answer2Button;
-    public Button answer3Button;
-    public Button answer4Button;
-    public Button answer5Button;
-    public Button answer6Button;
-    public Button answer7Button;
     //public Button nextButton;
     //public GameObject RandomAddGameObjects;
-
-
-    public int randomFirstNumber;
-    public int randomSecondNumber;
-     int[] options=new int[7]; 
 
     // public int randomFirstNumber2;
     // public int randomSecondNumber2;
@@ -40,16 +53,14 @@ public class AdditionPractice : MonoBehaviour
 
     // public int randomFirstNumber6;
     // public int randomSecondNumber6;
-
     
     int answerOne;
     int answerTwo;
     int answerThree;
-    int answerfour;
-    int answerfive;
-    int answersix;
-    int answerseven;
-    public int correctAnswer;
+    int answerFour;
+    int answerFive;
+    int answerSix;
+
     //public Text rightorwrong_Text;
 
     // public AudioSource correctAnswerAudio;
@@ -66,8 +77,9 @@ public class AdditionPractice : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-         //Random.InitState(System.DateTime.Now.Millisecond);
-         DisplayMathProblem();
+        //Random.InitState(System.DateTime.Now.Millisecond);
+        helperFunctions = FindObjectOfType<HelperFunctions>();
+        DisplayMathProblem();
     }
 
     // Update is called once per frame
@@ -75,71 +87,66 @@ public class AdditionPractice : MonoBehaviour
     {
         
     }
-
-    private (int, int) GetTwoRandomSum(){
-        int firstNumber=Random.Range(1, 10);
-        int secondNumber=Random.Range(1, 10 - firstNumber);
-
-        return(firstNumber,secondNumber);
-    }
-
-    public int[] GetRandomOptions(int firstNum, int secondnum){
-
-        int answer = firstNum + secondnum;
-
-        if (answer < 2 || answer > 10)
-        {
-            throw new System.Exception("GetSumOptions received invalid values to sum");
-        }       
-             
-        options[0] = answer;
-        for(int i=1;i<7;i++){
-        int option=Random.Range(1,10);        
-        options[i]=option;
-        } 
-        
-       
-        for (int i = 0; i < 7; i++)
-    {
-        int rand = Random.Range(i, 7);        
-        int tmp = options[i];
-        options[i] = options[rand];
-        options[rand] = tmp;
-        }
-              
-        return options;
-    }
      
-    public void DisplayMathProblem(){
+    public void DisplayMathProblem()
+    {
+        // Set up all 6 problems
+        var nums1 = helperFunctions.GetTwoRandomSum(9);
+        panel1firstNumber.text = "" + nums1.Item1;
+        panel1secondNumber.text = "" + nums1.Item2;
+        answerOne = nums1.Item1 + nums1.Item2;
 
-        var nums = GetTwoRandomSum();
-        randomFirstNumber = nums.Item1;
-        randomSecondNumber = nums.Item2;
-        
-        firstNumber.text = "" + randomFirstNumber;
-        secondNumber.text = "" + randomSecondNumber;         
+        var nums2 = helperFunctions.GetTwoRandomSum(9);
+        panel2firstNumber.text = "" + nums2.Item1;
+        panel2secondNumber.text = "" + nums2.Item2;
+        answerTwo = nums2.Item1 + nums2.Item2;
 
-       int[] numbers=new int[7];
-       numbers=GetRandomOptions(randomFirstNumber, randomSecondNumber);
-       answerOne = numbers[0];
-       answerTwo = numbers[1];
-       answerThree = numbers[2];
-       answerfour = numbers[3];
-       answerfive = numbers[4];
-       answersix = numbers[5];
-       answerseven = numbers[6];      
+        var nums3 = helperFunctions.GetTwoRandomSum(9);
+        panel3firstNumber.text = "" + nums3.Item1;
+        panel3secondNumber.text = "" + nums3.Item2;
+        answerThree = nums3.Item1 + nums3.Item2;
 
-       answer1Button.GetComponentInChildren<Text>().text = "" + answerOne;
-       answer2Button.GetComponentInChildren<Text>().text = "" + answerTwo;
-       answer3Button.GetComponentInChildren<Text>().text = "" + answerThree;
-       answer4Button.GetComponentInChildren<Text>().text = "" + answerfour;
-       answer5Button.GetComponentInChildren<Text>().text = "" + answerfive;
-       answer6Button.GetComponentInChildren<Text>().text = "" + answersix;
-       answer7Button.GetComponentInChildren<Text>().text = "" + answerseven;
-       int randomSum = randomFirstNumber+randomSecondNumber;
-       correctAnswer = randomSum;
+        var nums4 = helperFunctions.GetTwoRandomSum(9);
+        panel4firstNumber.text = "" + nums4.Item1;
+        panel4secondNumber.text = "" + nums4.Item2;
+        answerFour = nums4.Item1 + nums4.Item2;
 
+        var nums5 = helperFunctions.GetTwoRandomSum(9);
+        panel5firstNumber.text = "" + nums5.Item1;
+        panel5secondNumber.text = "" + nums5.Item2;
+        answerFive = nums5.Item1 + nums5.Item2;
 
+        var nums6 = helperFunctions.GetTwoRandomSum(9);
+        panel6firstNumber.text = "" + nums6.Item1;
+        panel6secondNumber.text = "" + nums6.Item2;
+        answerSix = nums6.Item1 + nums6.Item2;
+
+        panel1ans.text = "" + answerOne;
+        panel2ans.text = "" + answerTwo;
+        panel3ans.text = "" + answerThree;
+        panel4ans.text = "" + answerFour;
+        panel5ans.text = "" + answerFive;
+        panel6ans.text = "" + answerSix;
+
+        // Set up answer options
+        var options = new List<int>() { answerOne, answerTwo, answerThree, answerFour, answerFive, answerSix, Random.Range(1,10) };
+        // Randomize their order
+        int count = options.Count;
+        for (int i = 0; i < count - 1; ++i)
+        {
+            int rand = Random.Range(i, count);
+            int tmp = options[i];
+            options[i] = options[rand];
+            options[rand] = tmp;
+        }
+
+        answer1Button.text = "" + options[0];
+        answer2Button.text = "" + options[1];
+        answer3Button.text = "" + options[2];
+        answer4Button.text = "" + options[3];
+        answer5Button.text = "" + options[4];
+        answer6Button.text = "" + options[5];
+        answer7Button.text = "" + options[6];
     }
     // public void ButtonAnswer1()
     // {
