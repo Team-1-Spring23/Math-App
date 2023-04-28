4c934bc72bdf090a3343cd2d9c817bf51271feef
zarhult <cglis001@ucr.edu>
2023-04-27 21:24:12 -0700

    Finish setting up layout of advanced addition practice

--- Assets/AdditionPractice.cs
+++ Assets/AdditionPractice.cs
@@ -9,7 +9,7 @@ public class AdditionPractice : MonoBehaviour
     // Access HelperFunctions
     private HelperFunctions helperFunctions;
 
-    public Text panel1firstNumber;
+    public Text panel1firstNumber; // Text for each digit in each problem
     public Text panel1secondNumber;
     public Text panel1ans;
     public Text panel2firstNumber;
@@ -28,38 +28,27 @@ public class AdditionPractice : MonoBehaviour
     public Text panel6secondNumber;
     public Text panel6ans;
 
-    public Text answer1Button;
+    public Text answer1Button; // Text of the answer buttons
     public Text answer2Button;
     public Text answer3Button;
     public Text answer4Button;
     public Text answer5Button;
     public Text answer6Button;
     public Text answer7Button;
-
-    //public Button nextButton;
-    //public GameObject RandomAddGameObjects;
-
-    // public int randomFirstNumber2;
-    // public int randomSecondNumber2;
-
-    // public int randomFirstNumber3;
-    // public int randomSecondNumber3;
-
-    // public int randomFirstNumber4;
-    // public int randomSecondNumber4;
-
-    // public int randomFirstNumber5;
-    // public int randomSecondNumber5;
-
-    // public int randomFirstNumber6;
-    // public int randomSecondNumber6;
     
-    int answerOne;
+    int answerOne; // Values of the correct sums
     int answerTwo;
     int answerThree;
     int answerFour;
     int answerFive;
     int answerSix;
+
+    public int panel1question; // Which number are we asking for - first, second, or third
+    public int panel2question;
+    public int panel3question;
+    public int panel4question;
+    public int panel5question;
+    public int panel6question;
 
     //public Text rightorwrong_Text;
 
@@ -90,7 +79,7 @@ public class AdditionPractice : MonoBehaviour
      
     public void DisplayMathProblem()
     {
-        // Set up all 6 problems
+        // Set up all 6 problems, first the digits being summed
         var nums1 = helperFunctions.GetTwoRandomSum(9);
         panel1firstNumber.text = "" + nums1.Item1;
         panel1secondNumber.text = "" + nums1.Item2;
@@ -121,6 +110,7 @@ public class AdditionPractice : MonoBehaviour
         panel6secondNumber.text = "" + nums6.Item2;
         answerSix = nums6.Item1 + nums6.Item2;
 
+        // Set answer text
         panel1ans.text = "" + answerOne;
         panel2ans.text = "" + answerTwo;
         panel3ans.text = "" + answerThree;
@@ -128,9 +118,117 @@ public class AdditionPractice : MonoBehaviour
         panel5ans.text = "" + answerFive;
         panel6ans.text = "" + answerSix;
 
+        // Randomly choose a number in each panel to hide
+        panel1question = Random.Range(1, 4);
+        panel2question = Random.Range(1, 4);
+        panel3question = Random.Range(1, 4);
+        panel4question = Random.Range(1, 4);
+        panel5question = Random.Range(1, 4);
+        panel6question = Random.Range(1, 4);
+
+        var options = new List<int>(); // List of answer options (will contain everything that gets hidden here)
+        switch (panel1question)
+        {
+            case 1:
+                panel1firstNumber.gameObject.SetActive(false);
+                options.Add(int.Parse(panel1firstNumber.text));
+                break;
+            case 2:
+                panel1secondNumber.gameObject.SetActive(false);
+                options.Add(int.Parse(panel1secondNumber.text));
+                break;
+            case 3:
+                panel1ans.gameObject.SetActive(false);
+                options.Add(int.Parse(panel1ans.text));
+                break;
+        }
+        switch (panel2question)
+        {
+            case 1:
+                panel2firstNumber.gameObject.SetActive(false);
+                options.Add(int.Parse(panel2firstNumber.text));
+                break;
+            case 2:
+                panel2secondNumber.gameObject.SetActive(false);
+                options.Add(int.Parse(panel2secondNumber.text));
+                break;
+            case 3:
+                panel2ans.gameObject.SetActive(false);
+                options.Add(int.Parse(panel2ans.text));
+                break;
+        }
+        switch (panel3question)
+        {
+            case 1:
+                panel3firstNumber.gameObject.SetActive(false);
+                options.Add(int.Parse(panel3firstNumber.text));
+                break;
+            case 2:
+                panel3secondNumber.gameObject.SetActive(false);
+                options.Add(int.Parse(panel3secondNumber.text));
+                break;
+            case 3:
+                panel3ans.gameObject.SetActive(false);
+                options.Add(int.Parse(panel3ans.text));
+                break;
+        }
+        switch (panel4question)
+        {
+            case 1:
+                panel4firstNumber.gameObject.SetActive(false);
+                options.Add(int.Parse(panel4firstNumber.text));
+                break;
+            case 2:
+                panel4secondNumber.gameObject.SetActive(false);
+                options.Add(int.Parse(panel4secondNumber.text));
+                break;
+            case 3:
+                panel4ans.gameObject.SetActive(false);
+                options.Add(int.Parse(panel4ans.text));
+                break;
+        }
+        switch (panel5question)
+        {
+            case 1:
+                panel5firstNumber.gameObject.SetActive(false);
+                options.Add(int.Parse(panel5firstNumber.text));
+                break;
+            case 2:
+                panel5secondNumber.gameObject.SetActive(false);
+                options.Add(int.Parse(panel5secondNumber.text));
+                break;
+            case 3:
+                panel5ans.gameObject.SetActive(false);
+                options.Add(int.Parse(panel5ans.text));
+                break;
+        }
+        switch (panel6question)
+        {
+            case 1:
+                panel6firstNumber.gameObject.SetActive(false);
+                options.Add(int.Parse(panel6firstNumber.text));
+                break;
+            case 2:
+                panel6secondNumber.gameObject.SetActive(false);
+                options.Add(int.Parse(panel6secondNumber.text));
+                break;
+            case 3:
+                panel6ans.gameObject.SetActive(false);
+                options.Add(int.Parse(panel6ans.text));
+                break;
+        }
+
         // Set up answer options
-        var options = new List<int>() { answerOne, answerTwo, answerThree, answerFour, answerFive, answerSix, Random.Range(1,10) };
-        // Randomize their order
+        // First pick a random number that is not a correct option, since we need to include 1 dummy option
+        var possibilities = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        foreach (var i in options)
+        {
+            possibilities.Remove(i);
+        }
+        int randomIndex = Random.Range(0, possibilities.Count);
+        int dummyAnswer = possibilities[randomIndex];
+        options.Add(dummyAnswer);
+        // Randomize order of options
         int count = options.Count;
         for (int i = 0; i < count - 1; ++i)
         {
@@ -139,7 +237,7 @@ public class AdditionPractice : MonoBehaviour
             options[i] = options[rand];
             options[rand] = tmp;
         }
-
+        // Finally, set answer button text
         answer1Button.text = "" + options[0];
         answer2Button.text = "" + options[1];
         answer3Button.text = "" + options[2];
@@ -147,6 +245,8 @@ public class AdditionPractice : MonoBehaviour
         answer5Button.text = "" + options[4];
         answer6Button.text = "" + options[5];
         answer7Button.text = "" + options[6];
+
+        // Now all is set up, and the correct answers are stored in the text of the digits that were hidden during setup
     }
     // public void ButtonAnswer1()
     // {
