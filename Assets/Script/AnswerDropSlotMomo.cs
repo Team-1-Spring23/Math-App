40b962089f823888bf4d8149ec86f3273d813f6f
Bharat Gupta <36244814+virat07@users.noreply.github.com>
2023-05-02 18:32:14 -0700

    build issue fixed

--- Assets/Script/AnswerDropSlotMomo.cs
+++ Assets/Script/AnswerDropSlotMomo.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
-using UnityEngine.Rendering.VirtualTexturing;
+using UnityEngine.Rendering;
 using UnityEngine.UI;
 
 public class AnswerDropSlotMomo : MonoBehaviour, IDropHandler
