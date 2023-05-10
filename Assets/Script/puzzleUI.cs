dc8da7d391c38a018272e09601485016d3409c95
zarhult <cglis001@ucr.edu>
2023-05-10 15:37:00 -0700

    Add interstitial ads when leaving homescreen

--- Assets/Script/puzzleUI.cs
+++ Assets/Script/puzzleUI.cs
@@ -5,10 +5,11 @@ using UnityEngine.SceneManagement;
 
 public class puzzleUI : MonoBehaviour
 {
+    private admob AdMob;
     // Start is called before the first frame update
     void Start()
     {
-        
+        AdMob = FindObjectOfType<admob>();
     }
 
     // Update is called once per frame
@@ -17,9 +18,19 @@ public class puzzleUI : MonoBehaviour
         
     }
     
-    public void puzzleButton(string scene){
-       
-       SceneManager.LoadScene(scene);
- 
+    public void puzzleButton(string scene)
+    {
+        if (SceneManager.GetActiveScene().name == "home_screen") // Show interstitial ad when moving from home screen to a game
+        {
+            AdMob.ShowInterstitialAd();
+            AdMob.interstitialAd.OnAdFullScreenContentClosed += () =>
+            {
+                SceneManager.LoadScene(scene);
+            };
+        }
+        else
+        {
+            SceneManager.LoadScene(scene);
+        }
     }
 }
--- Assets/admob.cs
+++ Assets/admob.cs
@@ -18,21 +18,27 @@ public class admob : MonoBehaviour
             // This callback is called once the MobileAds SDK is initialized.
         });
 
-        LoadAd();
-        ListenToAdEvents();
+        LoadBannerAd();
+        LoadInterstitialAd();
+        ListenToBannerAdEvents();
+        ListenToInterstitialAdEvents(interstitialAd);
     }
 
 
     // These ad units are configured to always serve test ads.
-    #if UNITY_ANDROID
-        private string _adUnitId = "ca-app-pub-3940256099942544/6300978111";
-    #elif UNITY_IPHONE
-        private string _adUnitId = "ca-app-pub-3940256099942544/2934735716";
-    #else
-        private string _adUnitId = "unused";
-    #endif
+#if UNITY_ANDROID
+        private string _adUnitIdBanner = "ca-app-pub-3940256099942544/6300978111";
+        private string _adUnitIdInterstitial = "ca-app-pub-3940256099942544/1033173712";
+#elif UNITY_IPHONE
+        private string _adUnitIdBanner = "ca-app-pub-3940256099942544/2934735716";
+        private string _adUnitIdInterstitial = "ca-app-pub-3940256099942544/4411468910";
+#else
+    private string _adUnitIdBanner = "unused";
+    private string _adUnitIdInterstitial = "unused";
+#endif
 
-    BannerView _bannerView;
+    private BannerView _bannerView;
+    public InterstitialAd interstitialAd;
 
     /// <summary>
     /// Creates a 320x50 banner at top of the screen.
@@ -48,13 +54,13 @@ public class admob : MonoBehaviour
         }
 
         // Create a 320x50 banner at top of the screen
-        _bannerView = new BannerView(_adUnitId, AdSize.Banner, AdPosition.TopLeft);
+        _bannerView = new BannerView(_adUnitIdBanner, AdSize.Banner, AdPosition.TopLeft);
     }
 
     /// <summary>
     /// Creates the banner view and loads a banner ad.
     /// </summary>
-    public void LoadAd()
+    public void LoadBannerAd()
     {
         // create an instance of a banner view first.
         if (_bannerView == null)
@@ -72,9 +78,64 @@ public class admob : MonoBehaviour
     }
 
     /// <summary>
+    /// Loads the interstitial ad.
+    /// </summary>
+    public void LoadInterstitialAd()
+    {
+        // Clean up the old ad before loading a new one.
+        if (interstitialAd != null)
+        {
+            interstitialAd.Destroy();
+            interstitialAd = null;
+        }
+
+        Debug.Log("Loading the interstitial ad.");
+
+        // create our request used to load the ad.
+        var adRequest = new AdRequest.Builder()
+                .AddKeyword("unity-admob-sample")
+                .Build();
+
+        // send the request to load the ad.
+        InterstitialAd.Load(_adUnitIdInterstitial, adRequest,
+            (InterstitialAd ad, LoadAdError error) =>
+            {
+              // if error is not null, the load request failed.
+              if (error != null || ad == null)
+                {
+                    Debug.LogError("interstitial ad failed to load an ad " +
+                                   "with error : " + error);
+                    return;
+                }
+
+                Debug.Log("Interstitial ad loaded with response : "
+                          + ad.GetResponseInfo());
+
+                interstitialAd = ad;
+            });
+    }
+
+    /// <summary>
+    /// Shows the interstitial ad.
+    /// </summary>
+    public void ShowInterstitialAd()
+    {
+        if (interstitialAd != null && interstitialAd.CanShowAd())
+        {
+            Debug.Log("Showing interstitial ad.");
+            interstitialAd.Show();
+        }
+        else
+        {
+            Debug.LogError("Interstitial ad is not ready yet.");
+        }
+    }
+
+
+    /// <summary>
     /// listen to events the banner may raise.
     /// </summary>
-    private void ListenToAdEvents()
+    private void ListenToBannerAdEvents()
     {
         // Raised when an ad is loaded into the banner view.
         _bannerView.OnBannerAdLoaded += () =>
@@ -114,6 +175,38 @@ public class admob : MonoBehaviour
         _bannerView.OnAdFullScreenContentClosed += () =>
         {
             Debug.Log("Banner view full screen content closed.");
+        };
+    }
+
+    private void ListenToInterstitialAdEvents(InterstitialAd ad)
+    {
+        // Raised when the ad is estimated to have earned money.
+        ad.OnAdPaid += (AdValue adValue) =>
+        {
+            Debug.Log(string.Format("Interstitial ad paid {0} {1}.",
+                adValue.Value,
+                adValue.CurrencyCode));
+        };
+        // Raised when an impression is recorded for an ad.
+        ad.OnAdImpressionRecorded += () =>
+        {
+            Debug.Log("Interstitial ad recorded an impression.");
+        };
+        // Raised when a click is recorded for an ad.
+        ad.OnAdClicked += () =>
+        {
+            Debug.Log("Interstitial ad was clicked.");
+        };
+        // Raised when an ad opened full screen content.
+        ad.OnAdFullScreenContentOpened += () =>
+        {
+            Debug.Log("Interstitial ad full screen content opened.");
+        };
+        // Raised when the ad failed to open full screen content.
+        ad.OnAdFullScreenContentFailed += (AdError error) =>
+        {
+            Debug.LogError("Interstitial ad failed to open full screen content " +
+                           "with error : " + error);
         };
     }
 
