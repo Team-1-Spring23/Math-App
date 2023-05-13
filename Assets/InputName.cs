445603f0529e08b68befec9b07668f14e59d27da
zarhult <cglis001@ucr.edu>
2023-05-12 18:01:39 -0700

    Print username to console

--- Assets/InputName.cs
+++ Assets/InputName.cs
@@ -2,10 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Networking;
 using TMPro;
 
 public class InputName : MonoBehaviour
 {
+    [SerializeField] private string serverLocation = "http://localhost:3001/api/user-data";
     public TMP_InputField nameField;
     // Start is called before the first frame update
     void Start()
@@ -20,7 +22,27 @@ public class InputName : MonoBehaviour
     }
     private void nextScreen(string name)
     {
-        //Debug.Log(name);
-        SceneManager.LoadScene("Loading_bar");
+        Debug.Log(name);
+        StartCoroutine(postData(name));
+        //SceneManager.LoadScene("Loading_bar");
+    }
+
+    private IEnumerator postData(string data)
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("120", data);
+        using (UnityWebRequest www = UnityWebRequest.Post(serverLocation, form))
+        {
+            yield return www.SendWebRequest();
+
+            if (www.result != UnityWebRequest.Result.Success)
+            {
+                Debug.Log(www.error);
+            }
+            else
+            {
+                Debug.Log("Form upload complete!");
+            }
+        }
     }
 }
